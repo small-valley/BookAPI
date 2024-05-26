6fad20b4fae88b2ac1e7f2cddf8bc43bb576bc87
small-valley <kuzan1123@gmail.com>
2024-05-26 22:11:38 +0000

    chore: delete one line

--- Book_Lambda/src/Startup.cs
+++ Book_Lambda/src/Startup.cs
@@ -31,7 +31,6 @@ public class Startup
   // This method gets called by the runtime. Use this method to add services to the container
   public void ConfigureServices(IServiceCollection services)
   {
-    //services.AddMvc();
     services.AddControllers();
 
     // services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
