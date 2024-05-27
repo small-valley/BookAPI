1b3b937e3328b1644f8fa32fba66752a1c7341f8
small-valley <kuzan1123@gmail.com>
2024-05-26 19:25:29 -0700

    feat: modify cookie option

--- Book_Lambda/src/Controllers/AuthController.cs
+++ Book_Lambda/src/Controllers/AuthController.cs
@@ -59,8 +59,8 @@ public class AuthController : ControllerBase
 
     Response.Cookies.Append("access_token", accessToken, cookieOptions);
     Response.Cookies.Append("refresh_token", refreshToken, cookieOptions);
-    return Ok("set cookie");
+
     // Redirect to the frontend home page
-    //return Redirect(_configuration["Frontend:SigninSuccessRedirectUri"]);
+    return Redirect(_configuration["Frontend:SigninSuccessRedirectUri"]);
   }
 }
